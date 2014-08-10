9c38e4c99bd9c52e6078609ff5be093cc9fe3629
photonios <photonios@outlook.com>
2014-08-10 18:36:15 +0200

    Added base class for platform specific service implementations

---
+++ pyservice/platform_base.py
@@ -0,0 +1,90 @@
+######################################################################################
+#
+#   This file is part of PyService.
+#
+#   PyService is free software: you can redistribute it and/or modify it under the
+#   terms of the GNU General Public License as published by the Free Software
+#   Foundation, version 2.
+#
+#   This program is distributed in the hope that it will be useful, but WITHOUT
+#   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+#   FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+#   details.
+#
+#   You should have received a copy of the GNU General Public License along with
+#   this program; if not, write to the Free Software Foundation, Inc., 51
+#   Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+#   Copyright: Swen Kooij (Photonios) <photonios@outlook.com>
+#
+#####################################################################################
+
+class PyServicePlatformBase(object):
+    """Base class for classes that implement service behaviour for a specific platform.
+
+    All classes that implement service behaviour should derive from this class
+    and implement the virtual methods. This class can be seen as a contract.
+
+    """
+
+    def start(self):
+        """Starts the service (if it's installed and not running).
+
+        Returns:
+            True when starting the service was a success and false when
+            it failed.
+        """
+
+        raise NotImplementedError('`start` not implemented in derived class')
+
+    def stop(self):
+        """Stops the service (if it's installed and running).
+
+        Returns:
+            True when stopping the service was a success and false
+            when it failed.
+        """
+
+        raise NotImplementedError('`stop` not implemented in derived class')
+
+    def install(self):
+        """Installs the service so it can be started and stopped (if it's not installed yet).
+
+        Returns:
+            True when installing the service was a success and false
+            when it failed.
+        """
+
+        raise NotImplementedError('`install` not implemented in derived class')
+
+    def uninstall(self):
+        """Uninstalls the service so it can no longer be used (if it's installed).
+
+        Returns:
+            True when installing the service was a success and false
+            when it failed.
+        """
+
+        raise NotImplementedError('`uninstall` not implemented in derived class')
+
+    def is_installed(self):
+        """Determines whether this service is installed on this system.
+
+        Returns:
+            True when this service is installed on this system and false
+            when it was not installed on this system.
+        """
+
+        raise NotImplementedError('`is_installed` not implemented in derived class')
+
+    def is_running(self):
+        """Determines whether this service is running on this system.
+
+        Returns:
+            True when this service is running on this system and false
+            when it was not running on this system.
+        """
+
+        raise NotImplementedError('`is_running` not implemented in derived class')
+
+
